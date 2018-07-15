f67e83a9e1ac2664c4e56fcc8e5cfe85d9f2f9fa
John Vandivier <john.vandivier@halfaker.com>
2018-07-15 14:36:35 -0400

    card content wip

--- ui/src/app/dashboard/dashboard.component.ts
+++ ui/src/app/dashboard/dashboard.component.ts
@@ -6,10 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
-  cards = [
-    { title: 'Card 1', cols: 2, rows: 1 },
-    { title: 'Card 2', cols: 1, rows: 1 },
-    { title: 'Card 3', cols: 1, rows: 2 },
-    { title: 'Card 4', cols: 1, rows: 1 }
+  arroCards = [
+    { title: 'Card 1', cols: 2, rows: 1, sText: 'it yo dashboard dawg' },
+    { title: 'Bank Account Balance', cols: 1, rows: 1, sText: 'bank balance dank balance amirite' },
+    { title: 'Card 3', cols: 1, rows: 2, sText: 'anon long card' },
+    { title: 'Card 4', cols: 1, rows: 1, sText: 'anon short card' }
   ];
 }
